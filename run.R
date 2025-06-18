1e7c5ba2718d73a855f3d87ce768a5dbe182dbe8
Eric Scott <scottericr@gmail.com>
2025-06-18 15:24:13 -0700

    map over all stations

--- run.R
+++ run.R
@@ -1,9 +1,12 @@
 library(azmetr)
 library(readr)
 library(dplyr)
-library(bench)
+library(purrr)
+library(stringr)
+library(fs)
+library(glue)
 
-station_info
+# station_info
 
 station_list <- read_csv("azmet-station-list.csv")
 station_list
@@ -15,3 +18,24 @@ legacy_daily
 
 legacy_hourly <- azmet_hourly_data_download(station_list, "Tucson")
 legacy_hourly
+
+# Run for all sites and save data out to CSV
+
+station_names <- station_list$stn
+out_dir <- dir_create("legacy")
+
+#daily
+purrr::walk(station_names, \(station) {
+  legacy_daily <- azmet_daily_data_download(station_list, station)
+  write_csv(
+    legacy_daily$obs_dyly,
+    path(out_dir, glue("obs_dyly-{snakecase::to_snake_case(station)}.csv"))
+  )
+  write_csv(
+    legacy_daily$obs_dyly_derived,
+    path(
+      out_dir,
+      glue("obs_dyly_derived-{snakecase::to_snake_case(station)}.csv")
+    )
+  )
+})
