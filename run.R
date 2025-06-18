75c5c29d75a754685e75db1bb39814bbde02b45a
Eric Scott <scottericr@gmail.com>
2025-06-18 15:30:52 -0700

    repeat for hourly data

--- run.R
+++ run.R
@@ -16,7 +16,7 @@ source("R/azmet_hourly_data_download.R")
 legacy_daily <- azmet_daily_data_download(station_list, "Tucson")
 legacy_daily
 
-legacy_hourly <- azmet_hourly_data_download(station_list, "Tucson")
+legacy_hourly <- azmet_hourly_data_download(station_list, "Mohave #2")
 legacy_hourly
 
 # Run for all sites and save data out to CSV
@@ -25,17 +25,41 @@ station_names <- station_list$stn
 out_dir <- dir_create("legacy")
 
 #daily
-purrr::walk(station_names, \(station) {
-  legacy_daily <- azmet_daily_data_download(station_list, station)
-  write_csv(
-    legacy_daily$obs_dyly,
-    path(out_dir, glue("obs_dyly-{snakecase::to_snake_case(station)}.csv"))
-  )
-  write_csv(
-    legacy_daily$obs_dyly_derived,
-    path(
-      out_dir,
-      glue("obs_dyly_derived-{snakecase::to_snake_case(station)}.csv")
+purrr::walk(
+  station_names,
+  \(station) {
+    legacy_daily <- azmet_daily_data_download(station_list, station)
+    write_csv(
+      legacy_daily$obs_dyly,
+      path(out_dir, glue("obs_dyly-{snakecase::to_snake_case(station)}.csv"))
     )
-  )
-})
+    write_csv(
+      legacy_daily$obs_dyly_derived,
+      path(
+        out_dir,
+        glue("obs_dyly_derived-{snakecase::to_snake_case(station)}.csv")
+      )
+    )
+  },
+  .progress = TRUE
+)
+
+#hourly
+purrr::walk(
+  station_names,
+  \(station) {
+    legacy_hourly <- azmet_hourly_data_download(station_list, station)
+    write_csv(
+      legacy_hourly$obs_hrly,
+      path(out_dir, glue("obs_hrly-{snakecase::to_snake_case(station)}.csv"))
+    )
+    write_csv(
+      legacy_hourly$obs_hrly_derived,
+      path(
+        out_dir,
+        glue("obs_hrly_derived-{snakecase::to_snake_case(station)}.csv")
+      )
+    )
+  },
+  .progress = TRUE
+)
